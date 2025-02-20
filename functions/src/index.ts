fc00e1a18252ccf33420ebc82218a0ed3fafd9ce
vyashh <vyashbhawan@gmail.com>
2025-02-20 16:46:22 +0100

    added members to generateSchedule()

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,31 +1,19 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
 import { onRequest } from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
 import { onDocumentCreated } from "firebase-functions/v2/firestore";
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
 
-const testExternalFunction = (value: string) => {
-  console.log(value);
+const generateSchedule = (event: any) => {
+  const data = event.data?.data();
+  const members = data.members;
+
+  console.log(members);
 };
 
-export const generateSchedule = onDocumentCreated("houses/{docId}", (event) => {
+export const createSchedule = onDocumentCreated("houses/{docId}", (event) => {
   const data = event.data?.data();
   if (!data?.schedule) {
     console.log("schedule is missing");
-
-    const members = !data?.members || [];
-
-    console.log(members);
-    testExternalFunction("triggering external function");
+    generateSchedule(event);
   }
   if (!data) return;
 
