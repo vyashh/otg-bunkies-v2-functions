aaf74fabae7bc832d08faa90a3d82b853cdccc12
vyashh <vyashbhawan@gmail.com>
2025-02-20 16:35:07 +0100

    started working on a function will generate a schedule based on total members and tasks made

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,13 +7,32 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-import {onRequest} from "firebase-functions/v2/https";
+import { onRequest } from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
-
+import { onDocumentCreated } from "firebase-functions/v2/firestore";
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const testExternalFunction = (value: string) => {
+  console.log(value);
+};
+
+export const generateSchedule = onDocumentCreated("houses/{docId}", (event) => {
+  const data = event.data?.data();
+  if (!data?.schedule) {
+    console.log("schedule is missing");
+
+    const members = !data?.members || [];
+
+    console.log(members);
+    testExternalFunction("triggering external function");
+  }
+  if (!data) return;
+
+  console.log(data);
+});
+
+export const helloWorld = onRequest((request, response) => {
+  logger.info("Hello logs!", { structuredData: true });
+  response.send("Hello from Firebase!");
+});
